5107921a3c6b611b53a06e2eacd1265341fde55b
Fa1F <151896638+Fa1F@users.noreply.github.com>
2023-12-10 14:39:30 +0700

    about

--- book.js
+++ book.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-    event.preventDefault();
-    window.location.href = "https://fa1f.github.io/YuruCamp/konten.html";
+
+    const form = document.getElementById('bookingForm');
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        window.location.href = "https://fa1f.github.io/YuruCamp/konten.html";
+    })
+
 });
